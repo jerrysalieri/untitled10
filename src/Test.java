b08d9de69df4ad70b5ccc2676259ced43263a616
jerrysalieri <112954102+jerrysalieri@users.noreply.github.com>
2022-09-19 22:30:41 +0200

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class Test {
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        boolean isTrue = true;
+        while (isTrue) {
+            try{
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Enter a test value:");
+                String input = sc.nextLine();
+                int test;
+                test = Integer.parseInt(input);
+                if (test != 0) {
+                    System.out.println("The value entered is not 0");
+                    if (test > 5) {
+                        System.out.println("The value entered is bigger than 5");
+                        if (test >= 10) {
+                            System.out.println("The value entered is bigger or equal to 10");
+                            if (test == 99) {
+                                isTrue = false;
+                            }
+                        }
+                    } else if (test < -5) {
+                        System.out.println("The value entered is smaller than -5");
+                        if (test <= -10) {
+                            System.out.println("The value entered is smaller or equal to -10");
+
+                        }
+                    }
+                } else if (test == 0) {
+                    System.out.println("The value entered is 0");
+                } else {
+                    System.out.println("idk how did you get here");
+                }
+            }catch (Exception e){
+                System.out.println("Enter valid value!");
+            }
+        }
+
+    }
 }
+
